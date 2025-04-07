3b8f77a011552063fb30fb8e9efdff907b9dfab2
sejun <sejun2.lee@gmail.com>
2025-04-07 18:25:24 +0900

    Git 연결

---
+++ KGA_OOPConsoleProject/KGA_OOPConsoleProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KGA_OOPConsoleProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("콘솔게임 만들기");
+        }
+    }
+}
